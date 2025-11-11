6699905b674ef9a4d763ba952844cef5f9ca2c0a
rawan <rawan.dev6@gmail.com>
2025-11-11 10:50:56 +0200

    Add library page

--- app.py
+++ app.py
@@ -1,16 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for, flash
+from models.book import Book
+from models.reader import Reader
 
 app = Flask(__name__)
+
+reader = Reader('Rawan')
 
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/add_book')
+@app.route('/library')
+def library():
+    return render_template("library.html", books = reader.books)
+
+@app.route('/add_book', methods=['POST'])
 def add_book():
-    return render_template('add_book.html')
+    title = request.form.get("title")
+    author = request.form.get("author")
+    total_pages = int(request.form.get('total_pages', 0))
+    genre = request.form.get("genre")
+
+    if title and author and total_pages > 0:
+        new_book = Book(title, author, total_pages, genre)
+        reader.add_book(new_book)
+    return redirect('library')
 
 
+@app.route('/update_progress/<string:title>', methods=['POST'])
+def update_progress(title):
+    pages = int(request.form.get('pages'))
+    book = next((b for b in reader.books if b.title == title),None)
+
+    if not book:
+        flash('Book not found.',"error")
+        return redirect(url_for('library'))
+    remaining_pages =book.total_pages - book.pages_read
+
+    if pages > remaining_pages:
+        flash(f"You only have {remaining_pages} pages left in '{book.title}'.", "error")
+        return redirect(url_for('library'))
+
+    reader.log_reading(title, pages)
+
+    if book.pages_read >= book.total_pages:
+        flash(f"Congratulations! You’ve completed '{book.title}'", "success")
+
+
+    return redirect(url_for('library'))
+
+
+@app.route('/delete-book/<string:title>')
+def delete_book(title):
+    reader.remove_book(title)
+    return redirect(url_for('library'))
 
 
 if __name__ == '__main__':
